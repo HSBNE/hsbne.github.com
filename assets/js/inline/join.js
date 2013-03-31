60505f0c382d870f1f00434fef04091fd9f6ee8e
Joshua Hogendorn <jhogendorn@gmail.com>
2013-04-01 00:41:34 +1000

    Update the Join form to show a success message onpage

--- assets/js/inline/join.js
+++ assets/js/inline/join.js
@@ -1,13 +1,36 @@
 $(document).ready(function () {
-  $('form.join').h5Validate({
+  var $form = $('form.join');
+  var $thanks = $('.thanks');
+  var $iframe = $('#iframe_joinform');
+
+  // Make the h5validate script work nicely with bootstrap
+  $form.h5Validate({
     errorClass: '',
     validClass: ''
   });
-  $('form.join').bind('formValidated', function (e, data) {
+  $form.bind('formValidated', function (e, data) {
     $(data.elements).each(function (index, el) {
-      $(el.element).closest('fieldset').toggleClass('error', !el.valid);
-      $(el.element).closest('fieldset').toggleClass('success', el.valid);
+      $(el.element).closest('.control-group').toggleClass('error', !el.valid);
+      $(el.element).closest('.control-group').toggleClass('success', el.valid);
     });
+  });
+  $form.find('input,textarea,select').bind('validated', function (e, data) {
+    $(data.element).closest('.control-group').toggleClass('error', !data.valid);
+    $(data.element).closest('.control-group').toggleClass('success', data.valid);
+  });
+
+  var formsubmitted = false;
+
+  $iframe.load(function (e) {
+    if (formsubmitted) {
+      $form.fadeOut(500, function () {
+        $thanks.fadeIn(500);
+      });
+    }
+  });
+
+  $form.submit(function (e) {
+    formsubmitted = true;
   });
 });
 
