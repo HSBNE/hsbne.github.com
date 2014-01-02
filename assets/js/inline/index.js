2a153beb19df3e17e71b8ec9fb528cddb8c29b77
Joshua Hogendorn <jhogendorn@gmail.com>
2014-01-02 23:41:49 +1000

    Integrates the new tumblr blog by nog into the website

--- assets/js/inline/index.js
+++ assets/js/inline/index.js
@@ -2,4 +2,33 @@ $(document).ready(function () {
   $('.carousel').carousel({
     interval: 5000
   })
+
+  var PostView = Backbone.View.extend({
+      tagName: 'div',
+      className: 'row span8',
+      render: function () {
+          this.$el.html(
+              _.template(
+                  $('#post_' + this.model.data.type ).html()
+              )(this.model)
+          );
+          $('#blogfeed').append(this.$el);
+      }
+  });
+
+  var url = 'http://hsbne.tumblr.com';
+  var limit = 2;
+
+  $.getJSON(url+'/api/read/json?callback=?',
+    {
+      limit: limit
+    },
+    function(data) {
+      $('#blogfeed').empty();
+      $.each(data.posts, function(i,post,list){
+        var view = new PostView({model: { data: post }});
+        view.render();
+      });
+    }
+  );
 });
