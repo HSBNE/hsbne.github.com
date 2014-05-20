dfa50698f01c1efe26f169fd7e19f32eadf5934c
Buzz <davidbuzz@gmail.com>
2014-05-20 21:24:30 +1000

    Update join.js

--- assets/js/inline/join.js
+++ assets/js/inline/join.js
@@ -31,21 +31,52 @@ $(document).ready(function () {
 
   $form.submit(function (e) {
     formsubmitted = true;
-    
-    var jqxhr = $.post( "http://porthack.hsbne.org:81/seltzer/make_new_user_return_unique_username.php", function() {
-alert( "success" );
-})
-.done(function() {
-alert( "second success" );
-})
-.fail(function() {
-alert( "error" );
-})
-.always(function() {
-alert( "finished" );
-});
+
+
+    // setup some local variables
+    var $form = $(this);
+    // let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+    // serialize the data in the form
+    var serializedData = $form.serialize();
+
+   // let's disable the inputs for the duration of the ajax request
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+
+// fire off the request to /form.php
+    request = $.ajax({
+        url: "http://porthack.hsbne.org:81/seltzer/make_new_user_return_unique_username.php",
+        type: "post",
+        data: serializedData
+    });
+
+ // callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // log a message to the console
+        console.log("Hooray, it worked!");
+    });
+
+    // callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // log the error to the console
+        console.error(
+            "The following error occured: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // reenable the inputs
+        $inputs.prop("disabled", false);
+    });
     
     
   });
 });
+
 
